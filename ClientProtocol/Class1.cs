e387646842292f68834c13c6d45f489bd0febd55
Boil <FRee22858@live.com>
2019-09-23 23:12:08 +0800

    NF[框架] 配置文件读取，协议解析框架

---
+++ ClientProtocol/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClientProtocol
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProtocolBuild/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Utility;
+
+namespace ProtocolBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            PathUtil.Init();
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
